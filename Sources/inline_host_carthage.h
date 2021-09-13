4c9c641de98c04865bce9b1c4ae4e48a241c9c49
cody <cody@inline.tw>
2021-09-13 12:15:15 +0800

    chore: add project files

---
+++ Sources/inline_host_carthage.h
@@ -0,0 +1,18 @@
+//
+//  inline_host_carthage.h
+//  inline-host-carthage
+//
+//  Created by cody on 2021/9/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for inline_host_carthage.
+FOUNDATION_EXPORT double inline_host_carthageVersionNumber;
+
+//! Project version string for inline_host_carthage.
+FOUNDATION_EXPORT const unsigned char inline_host_carthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <inline_host_carthage/PublicHeader.h>
+
+
